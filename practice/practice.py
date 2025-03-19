f8c7463ebe5cecbb607858e22d83070691261a70
Manoj Kumar <mu.kumaran1991@gmail.com>
2025-03-19 18:41:41 +0530

    new

--- practice/practice.py
+++ practice/practice.py
@@ -1,17 +1,22 @@
-import random
-import math
+first_name = "manoj"
+last_name = "kumar"
+full_name = " ".join([first_name,last_name])
+age = 33
+print("Hello",first_name+last_name)
+print("Hello","+".join([first_name,last_name]))
 
-print(math.inf)
-print(-math.inf)
-print("")
+string1 = "Hello {}. You are {} years old".format(full_name,age)
+string2 = "Hello {fname}. You are {age} years old".format(fname=full_name,age=age)
+string3 = "Hello {0}. You are {1} years old".format(full_name,age)
+string4 = f"Hello {full_name}. You are {age} years old"
+string5 = f"Hello {first_name+" "+last_name}. You are {age+45} years old"
 
+print(string1)
+print(string2)
+print(string3)
+print(string4)
+print(string5)
 
-print(random.random())
-print(random.randint(100,150))
-print(random.randrange(100,150,5))
-
-lst = [12,34,45,67,89,92]
-print(l)
-print(random.choice(lst))
-random.shuffle(lst)
-print(l)
+name = full_name
+print(name[0],name[1],name[2],name[3],name[4],name[5],name[6])
+print(name[-1],name[-2],name[-3],name[-4],name[-5],name[-6])
