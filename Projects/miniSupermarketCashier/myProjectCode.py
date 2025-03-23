dd0d8dabb24986d671701ca681c5821374baab04
Manoj Kumar <mu.kumaran1991@gmail.com>
2025-03-23 16:36:43 +0530

    new addition

--- Projects/miniSupermarketCashier/myProjectCode.py
+++ Projects/miniSupermarketCashier/myProjectCode.py
@@ -1,22 +1,79 @@
 # Supermarket cashier
+# Membership discount will be given if billamount exceeds Rs.500
 
-# Accepting the product 
+# Functions
+def billAmount(product,prod_details):
+    billAmount = 0
+    print("Bill details:")
+    print("------------")
+    for key,value in product.items():
+        print(key.capitalize(),"= Rs.",prod_details[key]," X ",value," = ",(prod_details[key])*value)
+        billAmount += (prod_details[key])*value
+    return billAmount
 
-def product_details(prod_data,prod_name,prod_qty):
-    prod_data.update({prod_name:prod_qty})
-    return prod_data
+def billDiscount(bill,mem_discount):
+    discount = 0
+    if bill>500:
+        print("You are selected for membership discount")
+        mem_access = input("Membership available?(y/n):")
+        if('n' in  mem_access or 'no' in mem_access):
+            return discount
+        else:
+            membership = (input("Enter your membership (Gold/Silver/Bronze):")).lower()
+            for key,value in mem_discount.items():
+                if membership == key:
+                    discount = value*0.01*bill
+                return discount
+    else:
+        return discount
 
-prod_data  = {} 
-option  = True
+# Main Program
+print("Welcome to Vilvam supermarket")
+print("-----------------------------")
+print("Our Products:")
+print(""" 
+        Biscuit: Rs.25,
+        Chicken: Rs.250 (per kg),
+        Egg: Rs.8,
+        Fish: Rs.100 (per kg),
+        Coke: Rs.50,
+        Bread: Rs.20,
+        Apple: Rs.100 (per kg),
+        Onion: Rs.45 (per kg)
+""")
+prod_details={
+    'biscuit':25,
+    'chicken':250,
+    'egg':8,
+    'fish':100,
+    'coke':50,
+    'bread':20,
+    'apple':100,
+    'onion':45
+}
+
+mem_discount={
+        'gold':20,
+        'silver':10,
+        'bronze':5
+    }
+
+# Product updation
+product = {}
+option = True
 while(option):
-    prod_name = input("Enter the product name: ")
-    prod_qty = int(input("Enter the quantity: "))
-    prod_details = product_details(prod_data,prod_name,prod_qty) 
-    Add_product = input("Add product(Y/N): ")
-    if(Add_product == 'N'):
-        break
-    elif(Add_product != 'A'):
-        print("Enter the correct option")
+    prod_input = (input("Add product:")).lower()
+    qty_input = int(input("Add quantity:"))
+    product.update({prod_input:qty_input})
+    contd = (input("continue? (y/n):")).lower()
+    if (('n' in contd) or ('no' in contd)):
+        option = False
+print(product)
 
-print("Product brought:")
-print(prod_details)
+# Calculating total
+bill = billAmount(product,prod_details)
+print("Bill amount:",bill)
+
+# Adding discount
+discount = billDiscount(bill,mem_discount)
+print("Discount: Rs.",discount)
