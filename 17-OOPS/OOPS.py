0782b1242d00b6aaefd858c6e51b01098e36126e
Manoj Kumar <mu.kumaran1991@gmail.com>
2025-03-31 19:04:05 +0530

    new addition

---
+++ 17-OOPS/OOPS.py
@@ -0,0 +1,63 @@
+class Employee_0:
+    name = "john"
+    age = 35
+    designation = "Manager"
+    
+emp0 = Employee_0()
+print(emp0.name)
+print(emp0.age)
+print(emp0.designation)
+
+# using constructors (__init__)
+class Employee_1:
+    def __init__(self):
+        self.name = "John"
+        self.age = 35
+        self.designation = "Manager"
+
+emp1 = Employee_1()
+print(emp1.name,emp1.age,emp1.designation)
+
+# using parameterized constructors
+class Employee_2:
+    def __init__(self,name,age,designation):
+        self.name = name
+        self.age = age
+        self.designation = designation
+
+emp_1 = Employee_2('Hari',45,'Sr.Manager')
+emp_2 = Employee_2('Vinoth',30,'Manager')
+print(emp_1.name,emp_1.age,emp_1.designation)
+print(emp_2.name,emp_2.age,emp_2.designation)
+
+# adding methods and behaviours
+
+class Employee:
+    totalEmployees = 0
+
+    def __init__(self,name,age,designation,salary):
+        self.name = name
+        self.age = age
+        self.designation = designation
+        self.salary = salary
+        Employee.totalEmployees += 1
+
+    def getEmployeeDetails(self):
+        return self.name,self.age,self.designation,self.salary
+    
+    def updateSalary(self,newSalary):
+        self.salary = newSalary
+        print("Salary updated")
+        return self.salary
+    
+empOne = Employee('Srinivasan',28,'Jr.developer',25000)
+print(empOne.getEmployeeDetails())
+empTwo = Employee('Nagulan',38,'Manager',40000)
+print(empTwo.getEmployeeDetails())
+empOne.updateSalary(30000)
+print(empOne.getEmployeeDetails())
+print(Employee.totalEmployees)
+        
+
+
+
--- practice/practice.py
+++ practice/practice.py
@@ -1,3 +1,39 @@
-fp = open("D:/Software/Livewire_notes/Python/practice/practice.txt",'r+')
-# fp.write("\nHello world! Python is the great language")
-print(fp.read())
+class School:
+    def __init__(self,name,city,student_strength,staff_strength,year):
+        self.name = name
+        self.year = year
+        self.city = city
+        self.staff_strength = staff_strength
+        self.student_strength = student_strength
+
+    def schoolDetails(self):
+        print("Schoolname:",self.name)
+        print("Year of Opening:",self.year)
+        print("City:",self.city)
+        print("No of staffs:",self.staff_strength)
+        print("No of students:",self.student_strength)
+        print("")
+        
+
+    def updateStaffStrength(self,newStaff):
+        self.staff_strength = newStaff
+        print("No of staffs updated as",self.staff_strength,"in",self.name)
+        return self.staff_strength
+    
+    def updateStudentStrength(self,newStudent):
+        self.student_strength = newStudent
+        print("No of students updated as",self.student_strength,"in",self.name)
+        return self.student_strength
+    
+
+kvv = School("Karur Vetri Vinayaka Higher Secondary School","Karur",3500,85,2014)
+kvv.schoolDetails()
+
+cheran = School("Cheran Matric Higher Secondary School","Karur",4000,80,1985)
+cheran.schoolDetails()
+
+kvv.updateStudentStrength(4500)
+kvv.updateStaffStrength(90)
+
+kvv.schoolDetails()
+
