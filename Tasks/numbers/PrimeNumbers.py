276d43cb7e36ff7c2f859948235df50375b4b1ae
Manoj Kumar <mu.kumaran1991@gmail.com>
2025-04-05 18:54:34 +0530

    new

--- Tasks/numbers/PrimeNumbers.py
+++ Tasks/numbers/PrimeNumbers.py
@@ -4,11 +4,21 @@ num = int(input("Enter the number:"))
 if(num <= 1):
     print("Its not a prime")
 else:
-    for i in range(2,num):
+    for i in range(2,num):   
         if(i**2<=num):
             if(num%i==0):
                 print(f"Its not a prime. It is divisible by {i}")
                 break
     else:
         print("Its a prime number")
+    
+
+# else:
+#     for i in range(2,int(num**0.5)+1):   
+
+#             if(num%i==0):
+#                 print(f"Its not a prime. It is divisible by {i}")
+#                 break
+#     else:
+#         print("Its a prime number")
     
