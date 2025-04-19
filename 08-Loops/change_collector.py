4a5940b92ef3dad527188c9f7c9b6f88b6b38681
Manoj Kumar <mu.kumaran1991@gmail.com>
2025-04-19 17:25:42 +0530

    new updation

--- 08-Loops/change_collector.py
+++ 08-Loops/change_collector.py
@@ -1,20 +1,37 @@
 # change collector
-
-coin1 = 1
-coin2 = 2
-coin3 = 5
+import math
+coins = [20,10,5,2,1]
 amt = int(input("Enter the amount for which you need a change: "))
-total = 0
-c1=c2=c3 = 0
-while(total<=amt):
-    if total<amt:
-        total  = total + coin1+coin2+coin3
-        c1 = c1 + coin1
-        c2 = c2 + coin2
-        c3 = c3 + coin3
-    elif total == amt:
-        break
-print("Change collected =",total)
-print("1 rupee -",c1/1,"coins")
-print("2 rupee -",c2/2,"coins")
-print("5 rupee -",c3/5,"coins")
+if(amt>coins[2]):
+    coin20 = math.floor(amt/coins[2])
+    bal3 = amt%coins[2]
+print(coin20,bal3)
+
+
+# total = 0
+# c1=c2=c3 = 0
+# while(total<=amt):
+#     if total<amt:
+#         total  = total + coin1+coin2+coin3
+#         c1 = c1 + coin1
+#         c2 = c2 + coin2
+#         c3 = c3 + coin3
+#     elif total == amt:
+#         break
+# print("Change collected =",total)
+# print("1 rupee -",c1/1,"coins")
+# print("2 rupee -",c2/2,"coins")
+# print("5 rupee -",c3/5,"coins")
+
+# algo:
+# eg: 256 rs
+# 1st step: if 256>=20
+# 256/20   Q = 12 coins bal= 16 rs
+# 2nd step: if 16>=10
+# 16/10     Q = 1 coin bal = 6 rs
+# 3rd step: if 6>=5
+# 6/5       Q = 1 coin bal = 1 rupee
+# 4th step: if 1>=2
+# skip 
+# 5th step: if 1>=1 
+# 1/1         Q = 1 coin bal = 0 rupee
--- practice/practice.py
+++ practice/practice.py
@@ -1,51 +1,5 @@
-# print("Prime number check")
-# num = int(input("Enter the number:"))
-# if(num <= 1):
-#     print("Its not a prime")
-# else:
-#     for i in range(2,int(num**0.5)+1):
-#             print(i)
-#             if(num%i==0):
-#                 print(f"Its not a prime. It is divisible by {i}")
-#                 break
-#     else:
-#         print("Its a prime number")
+i=1
+while(i<6):
+    print(i*"* ")
+    i += 1
 
-num = 8
-print(num**0.5)
-print(int(num**0.5))
-
-def add(a=2,b=30):
-    return a+b
-
-def iseven(a):
-    if a%2==0:
-        return True
-    
-print(add(5,2))
-print(add())
-
-square = lambda n=3:n*n
-addi = lambda a=2,b=3: a+b
-
-print("lambda functions")
-print(square(4))
-print(square(4.5))
-print(square())
-print(addi(10,50))
-print(addi())
-
-print("map,filter,reduce functions")
-
-from functools import reduce
-num = list(range(1,11))
-print(num)
-
-A = list(map(square,num))
-print("squared numbers:",A)
-
-B = list(filter(iseven,num))
-print("Even numbers:",B)
-
-C = reduce(addi,num)
-print("Summation:",C)
