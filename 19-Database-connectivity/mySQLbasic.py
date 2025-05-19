7aba11235f71f8a5492eed63941638a2c6021d21
mu-kumaran <mu.kumaran1991@gmail.com>
2025-05-19 18:38:25 +0530

    new commit

--- 19-Database-connectivity/mySQLbasic.py
+++ 19-Database-connectivity/mySQLbasic.py
@@ -4,7 +4,20 @@ import mysql.connector
 myDB = mysql.connector.connect(
     host = "localhost",
     user="root",
-    password = "hWsjr8+i#"
+    password = "xxxxxxxxx",
+    database = "pythonx"
 )
 
 print(myDB)
+
+# DATABASE INTERACTION
+# myCur = myDB.cursor()
+# # myCur.execute("CREATE DATABASE PYTHONX")
+# # myCur.execute("DROP DATABASE employee")
+# # myCur.execute("SHOW DATABASES")
+# myDB.close() 
+
+# CREATING TABLES
+# cur = myDB.cursor()
+# cur.execute("CREATE TABLE user(userId int PRIMARY KEY, username varchar(10), email varchar(25), userType varchar(10))") 
+# myDB.close() 
