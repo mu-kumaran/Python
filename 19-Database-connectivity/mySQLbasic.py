59d79c8e7b55dd3289eb55e9229c392cf38d3195
mu-kumaran <mu.kumaran1991@gmail.com>
2025-05-19 19:14:38 +0530

    new commit

--- 19-Database-connectivity/mySQLbasic.py
+++ 19-Database-connectivity/mySQLbasic.py
@@ -4,7 +4,7 @@ import mysql.connector
 myDB = mysql.connector.connect(
     host = "localhost",
     user="root",
-    password = "xxxxxxxxx",
+    password = "xxxxxxx",
     database = "pythonx"
 )
 
@@ -21,3 +21,14 @@ print(myDB)
 # cur = myDB.cursor()
 # cur.execute("CREATE TABLE user(userId int PRIMARY KEY, username varchar(10), email varchar(25), userType varchar(10))") 
 # myDB.close() 
+
+# Inserting and fetching data
+cur = myDB.cursor()
+cur.execute('INSERT INTO user VALUES(1526,"John","john@example.com","PRO")') 
+cur.execute('INSERT INTO user VALUES(6745,"Smith","Smith@example.com","REGULAR")') 
+myDB.commit()  # to save and get reflected in database
+cur.execute("SELECT * FROM user")
+result = cur.fetchall()
+for i in result:
+    print(i)
+myDB.close() 
